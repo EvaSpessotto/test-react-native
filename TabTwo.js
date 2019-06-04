241d08fbaed6b2df2718bf811a02439fe6f3e9f1
EvaSpessotto <eva@spessotto.com>
2019-06-04 14:27:05 +0200

    Add accelerometer

--- TabTwo.js
+++ TabTwo.js
@@ -3,7 +3,7 @@ import { StyleSheet, View, Button, Alert, Text } from "react-native";
 
 export default class TabTwo extends React.Component {
   static navigationOptions = {
-    tabBarLabel: "Onglet deux"
+    tabBarLabel: "Pouet"
   };
 
   render() {
